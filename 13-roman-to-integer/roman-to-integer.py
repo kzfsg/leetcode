b9ade1ba9599f63aa6d7811857da740ee7f0bdbe
kzfsg <131367782+kzfsg@users.noreply.github.com>
2024-02-02 10:49:11 +0800

    Time: 36 ms (34.21%) | Memory: 11.8 MB (84.00%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def romanToInt(self, s):
+        romanDictionary = {
+            'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000
+        }
+        total = 0
+        listedString = list(s)
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and romanDictionary[s[i]] < romanDictionary[s[i+1]]:
+                total -= romanDictionary[s[i]]
+            else:
+                total += romanDictionary[s[i]]
+        return total
+
+
+
+
+        

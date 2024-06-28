0db71bc51724e557f0cb15982df1cf728b213861
kzfsg <131367782+kzfsg@users.noreply.github.com>
2024-06-28 16:24:33 +0800

    Time: 17 ms (70.82%) | Memory: 11.4 MB (99.38%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,43 +1,42 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
+        nums1clone = nums1[:]
         x = 0
         y = 0
         z = 0
-        t = m + n
-        nums1clone = nums1[:]
+        
         while x < m and y < n:
             if nums1clone[x] > nums2[y]:
                 nums1[z] = nums2[y]
-                y += 1 
+                y += 1
                 z += 1
+
             elif nums1clone[x] < nums2[y]:
                 nums1[z] = nums1clone[x]
                 x += 1
                 z += 1
+
             elif nums1clone[x] == nums2[y]:
                 nums1[z] = nums1clone[x]
-                nums1[z+1] = nums2[y]
-                z += 2
+                nums1[z + 1] = nums2[y]
                 x += 1
                 y += 1
-
-        if x != m:
-            while x < m:
+                z += 2
+        
+        while x + y < m + n:
+            if x < m:
                 nums1[z] = nums1clone[x]
-                z += 1
                 x += 1
-        elif y != n:
-            while y < n:
+                z += 1
+            elif y < n:
                 nums1[z] = nums2[y]
                 y += 1
                 z += 1
-        
+
         return nums1
-            
-                    
-                    
+
+
         
-        # 1. compare first integer in both arrays, keep larger integer and compare with next in array with smaller integer.
-        # 2. to do so, initialise another array which copies nums1
-        # 3. keep going with the comparison until length m+n is achieved for nums1
-        # use while loop if youre comparing two arrays side by side.
+        # 1. Clone nums1
+        # 2. Compare nums1clone & nums2, altering nums1 and storing the numbers in order in nums1
+        # 3. Repeat until length m+n for nums1 is achieved. If not, create a clause to account for the remaining numbers and append them to no.1

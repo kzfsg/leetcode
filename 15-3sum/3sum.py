7a551b26ad75d86bf13dadb02b160fdc80ae14d9
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-06-01 13:12:16 +0800

    Time: 898 ms (32.60%) | Memory: 18.5 MB (41.03%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -7,20 +7,26 @@ class Solution(object):
         sortedNums = sorted(nums)
         solutionList = []
 
-        for index, num in enumerate(sortedNums):
-            twoSum = {}
-            target = 0 - num
+        for index in range(len(sortedNums)):
+            target = 0 - sortedNums[index]
             if index > 0 and sortedNums[index] == sortedNums[index-1]:
                 continue 
-            for index2 in range(index + 1, len(sortedNums)):  # Only look forward!
-                complement = sortedNums[index2]
-                if index == index2:
-                    continue
-                if complement in twoSum:
-                    solution = sorted([num, twoSum[complement], complement])
-                    if solution not in solutionList:
-                        solutionList.append(solution)                    
-                twoSum[target - complement] = complement # store complement as key, value as value
+            secondP = index + 1
+            thirdP = len(nums) - 1
+            
+            while secondP < thirdP:
+                curr = sortedNums[secondP] + sortedNums[thirdP]
+                if curr > target:
+                    thirdP -= 1
+                elif curr < target:
+                    secondP += 1
+                
+                else:
+                    solutionList.append([sortedNums[index], sortedNums[secondP], sortedNums[thirdP]])
+                    secondP += 1
+
+                    while sortedNums[secondP] == sortedNums[secondP-1] and secondP < thirdP:
+                        secondP += 1
 
         print(solutionList)
         return solutionList

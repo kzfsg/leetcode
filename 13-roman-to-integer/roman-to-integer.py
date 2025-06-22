73c61e3fbf10c103e6a8096ba7b19f8e202654b2
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-06-22 11:38:19 +0800

    Time: 9 ms (53.64%) | Memory: 12.4 MB (83.45%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,18 +1,22 @@
 class Solution(object):
     def romanToInt(self, s):
-        romanDictionary = {
-            'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000
+        """
+        :type s: str
+        :rtype: int
+        """
+        hashTable = {
+        'I': 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
         }
+
         total = 0
+        i = 0
 
-        for i in range(len(s)):
-            if i + 1 < len(s) and romanDictionary[s[i]] < romanDictionary[s[i+1]]:
-                total -= romanDictionary[s[i]]
+        while i < len(s):
+            if i + 1 < len(s) and hashTable[s[i]] < hashTable[s[i+1]]:
+                total += hashTable[s[i+1]] - hashTable[s[i]]
+                i += 2
             else:
-                total += romanDictionary[s[i]]
+                total += hashTable[s[i]]
+                i += 1
+                
         return total
-
-
-
-
-        
